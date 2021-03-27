d79e15c232e5dc9b7671de0bd425e88d870b9b6f
Faris Kadir <faris.kadir@gmail.com>
2021-03-26 21:46:41 -0400

    Updated the profile view and js files as well as the login view.

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,13 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#post-title').value.trim();
-  const description = document.querySelector('#post-content').value.trim();
+  const title = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-content').value.trim();
 
-  if (name && needed_funding && description) {
+  if (title && content) {
     const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, content }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -38,7 +38,7 @@ const delButtonHandler = async (event) => {
 };
 
 document
-  .querySelector('.new-project-form')
+  .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
 document
